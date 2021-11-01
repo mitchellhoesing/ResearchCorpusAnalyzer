a5c848866eb3fcbd18dcda96d36ca37314bebfbc
dodgej <antisheep7@gmail.com>
2021-11-01 16:40:09 -0700

    added git ignore and messed with paths

--- seleniumDataScraper/main.py
+++ seleniumDataScraper/main.py
@@ -67,12 +67,12 @@ options = webdriver.ChromeOptions()
 options.add_argument('--ignore-certificate-errors')
 options.add_argument("--test-type")
 options.add_experimental_option('prefs', {
-"download.default_directory": r"C:\Users\hoesi\PycharmProjects\CorpusAnalyzer\seleniumDataScraper\papers", #Change default directory for downloads
+"download.default_directory": r"./papers", #Change default directory for downloads
 "download.prompt_for_download": False, #To auto download the file
 "download.directory_upgrade": True,
 "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
 })
-driver = webdriver.Chrome(executable_path=r"C:\chromedriver_win32\chromedriver.exe", options=options)
+driver = webdriver.Chrome(executable_path=r"C:/chromedriver_win32/chromedriver.exe", options=options)
 
 #Open the paper and immediately download.
 #driver.get('https://dl.acm.org/doi/pdf/10.1145/3313831.3376131')
@@ -84,7 +84,7 @@ driver = webdriver.Chrome(executable_path=r"C:\chromedriver_win32\chromedriver.e
 # driver.get(link)
 
 # Extract plain text from pdf
-raw = parser.from_file('./proceedings/chi2019-papers/1proc/paper004.pdf')
+raw = parser.from_file('./papers/test.pdf')
 # print(raw['content'])
 f = open("test2019.txt", "w")
 encodedText = raw['content'].encode("ascii", "ignore")
