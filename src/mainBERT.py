34298fb27cb4dd4e728754e1d562f2ac802a3381
Mitch <hoesingmitch02@gmail.com>
2021-11-15 18:32:28 -0800

    cuda is working!

--- src/mainBERT.py
+++ src/mainBERT.py
@@ -1,5 +1,7 @@
 import os
 import re
+
+from torch.autograd import Variable
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 import torch
@@ -11,6 +13,15 @@ if ".DS_Store" in filenameList:
 if ".gitignore" in filenameList:
     filenameList.remove(".gitignore")
 filenameList.sort()
+
+# Initialize BERT
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased-finetuned-mrpc")
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased-finetuned-mrpc")
+classes = ["not paraphrase", "is paraphrase"]
+if torch.cuda.is_available():
+    model.cuda()
+else:
+    print("You are not using Cuda Cores.")
 
 # Per file
 for filename in filenameList:
@@ -33,9 +44,11 @@ for filename in filenameList:
     # Types of studies: Brainstorming and focus group, interviews, questionnaires, think aloud sessions, instrumenting systems, fly on the wall, analysis of tool use logs, static and dynamic analysis
     # Sources of data: social media, interviews,
     # What are good paraphrases?
+
     # TODO
     # 0) add newlines to regex to break on. This is being a pain in the ass, do we care? Relevant sentences should be delimited by a "."
     # 1) Store all data results and graph a histogram.
+    # 2) list: year, title ,doi ,max , argmax,list of scores
     # Results:
     # Only print results after each paper, not each sentence.
     # List containing: Year, Title, DOI, result list
@@ -52,16 +65,15 @@ for filename in filenameList:
 
     # Per paraphrase
     for iParaphrases in range(len(paraphrases)):
-        # Initialize BERT
-        tokenizer = AutoTokenizer.from_pretrained("bert-base-cased-finetuned-mrpc")
-        model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased-finetuned-mrpc")
-        classes = ["not paraphrase", "is paraphrase"]
 
         results.append(paraphrases[iParaphrases])
         # Per sentence in a paper
         for iSentences in range(len(sentences)):
             # Tokenize and encode into a tensor.
             paraphrase = tokenizer.encode_plus(paraphrases[iParaphrases], sentences[iSentences], padding=True, return_tensors="pt")
+
+            if torch.cuda.is_available():
+                paraphrase = paraphrase.to('cuda')
 
             # Classify the tensor using a logistic regression model.
             paraphraseClassificationLogits = model(**paraphrase)[0]
@@ -88,6 +100,6 @@ for filename in filenameList:
         print(f"Highest: {highestPercent}% {highestProbabilityIsParaphrase}")
 
         results.append(highestProbabilityIsParaphrase + ": " + str(highestPercent) +"%")
-        print(results)
+
     for i in range(len(results)):
         print(results[i], end="\t")
